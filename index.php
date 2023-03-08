0643917bfdb8ec2f4241ca06131284a49520c749
JUAN VITOR TASCA DE ANDRADE <juan.4910@aluno.pr.senac.br>
2023-03-08 11:43:00 -0300

    atualizar

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+    $pessoa1 = array ();
+    $pessoa2 = array ();
+    $pessoa3 = array ();
+    $pessoa4 = array ();
+
+    $pessoa1 [0] [0] [0] [0] [0] ["nome"] = "João Pedro Fernandes";
+    $pessoa1 [0] [0] [0] [0] [0] ["idade"] = "456";
+    $pessoa1 [0] [0] [0] [0] [0] ["casa"] = "Grifinória";
+
+    $pessoa2 [1] [0] [0] [0] [0] ["nome"] = "Joaquim";
+    $pessoa2 [1] [0] [0] [0] [0] ["idade"] = "Indefinido";
+    $pessoa2 [1] [0] [0] [0] [0] ["casa"] = "Grifinória";
+
+    $pessoa3 [2] [0] [0] [0] [0] ["nome"] = "Malu";
+    $pessoa3 [2] [0] [0] [0] [0] ["idade"] = "17";
+    $pessoa3 [2] [0] [0] [0] [0] ["casa"] = "Sonserina";
+
+    $pessoa4 [3] [0] [0] [0] [0] ["nome"] = "Jraffis";
+    $pessoa4 [3] [0] [0] [0] [0] ["idade"] = "12";
+    $pessoa4 [3] [0] [0] [0] [0] ["casa"] = "Lufa-Lufa";
+
+    echo ("O " . $pessoa1 [0] [0] [0] [0] [0] ["nome"] . " tem " . $pessoa1 [0] [0] [0] [0] [0] ["idade"] . " anos e é da casa de " . $pessoa1 [0] [0] [0] [0] [0] ["casa"]); 
+    echo ("</br>");
+    echo ("O " . $pessoa2 [1] [0] [0] [0] [0] ["nome"] . " tem " . $pessoa2 [1] [0] [0] [0] [0] ["idade"] . " anos e é da casa de " . $pessoa2 [1] [0] [0] [0] [0] ["casa"]);
+    echo ("</br>");
+    echo ("O " . $pessoa3 [2] [0] [0] [0] [0] ["nome"] . " tem " . $pessoa3 [2] [0] [0] [0] [0] ["idade"] . " anos e é da casa de " . $pessoa3 [2] [0] [0] [0] [0] ["casa"]);
+    echo ("</br>");
+    echo ("O " . $pessoa4 [3] [0] [0] [0] [0] ["nome"] . " tem " . $pessoa4 [3] [0] [0] [0] [0] ["idade"] . " anos e é da casa de " . $pessoa4 [3] [0] [0] [0] [0] ["casa"]);
+
+
+    
+?>
